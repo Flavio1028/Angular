3ba11436b0c0c080f1b9a0bf8f930bb884acd05e
Flávio Rocha <flavio-10_28@hotmail.com>
2021-12-25 12:17:05 -0300

    Criação da classe Course Service

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -9,14 +10,13 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', 'category': 'front-end' }
-  ];
+  courses: Course[] = []
   displayedColumns: string[] = ['name', 'category']
 
-  constructor() { }
+  constructor(private service:CoursesService) { }
 
   ngOnInit(): void {
+    this.courses = this.service.list();
   }
 
 }
