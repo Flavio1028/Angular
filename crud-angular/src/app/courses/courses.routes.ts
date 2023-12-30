e9c994d6fcc13a861acf26a6077907409d5e8fe8
Flávio Rocha <flavio-10_28@hotmail.com>
2023-12-30 10:08:11 -0300

    migração para staandalone

--- crud-angular/src/app/courses/courses.routes.ts
+++ crud-angular/src/app/courses/courses.routes.ts
@@ -1,19 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CoursesResolver } from './guards/courses.resolver';
+import { Routes } from '@angular/router';
 
 import { CourseFormComponent } from './containers/course-form/course-form.component';
 import { CoursesComponent } from './containers/courses/courses.component';
+import { CoursesResolver } from './guards/courses.resolver';
 
-
-const routes: Routes = [
+export const COURSES_ROUTES: Routes = [
   { path: '', component: CoursesComponent },
   { path: 'new', component: CourseFormComponent, resolve: { course: CoursesResolver}  },
   { path: 'edit/:id', component: CourseFormComponent, resolve: { course: CoursesResolver} }
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class CoursesRoutingModule { }
